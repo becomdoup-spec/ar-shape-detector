f6b8941ce23388f312e1777eaf47767dd28fd9f3
becomdoup-spec <becomdoup@gmail.com>
2026-01-06 20:38:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+cv['onRuntimeInitialized'] = () => {
+  console.log("OpenCV is ready");
+  startCamera();
+};
+
 navigator.mediaDevices.getUserMedia({
   video: { facingMode: "environment" }
 }).then(stream => {
